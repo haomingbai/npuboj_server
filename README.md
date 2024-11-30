c0afb1eda2d2e106f7bbdf9d06ef142a4d9a6b41
haomingbai <haomingbai@hotmail.com>
2024-12-01 00:49:15 +0800

    first commit, new project

---
+++ README.md
@@ -0,0 +1,1 @@
+# npuboj_server
---
+++ build/CMakeFiles/npuboj_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/npuboj_server.dir/link.d"
+  "CMakeFiles/npuboj_server.dir/main.cpp.o"
+  "CMakeFiles/npuboj_server.dir/main.cpp.o.d"
+  "npuboj_server"
+  "npuboj_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/npuboj_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
